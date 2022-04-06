3faf45febbde37959fd91247ac7ed876205ab802
Vishal Choudhary <contactvishaltech@gmail.com>
2022-04-06 11:15:47 +0530

    spot controller updated

--- src/spot/spot.controller.ts
+++ src/spot/spot.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { reserverSpotDto } from './dto';
+import { freeSpotDto } from './dto/free-spot.dto';
+import { SpotService } from './spot.service';
 
 @Controller('spot')
-export class SpotController {}
+export class SpotController {
+  constructor(private spotService: SpotService) {}
+
+  @Post('reserve')
+  reserve(@Body() dto: reserverSpotDto) {
+    this.spotService.reserve(dto);
+  }
+
+  @Get('freespots')
+  freeSpots(@Body() dto: freeSpotDto) {
+    this.spotService.freespots(dto);
+  }
+}
